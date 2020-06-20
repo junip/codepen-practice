bc4d7b820960d3d52adf3f400f3c1a95624b889e
Junip Dewan <junipd2@gmail.com>
2020-06-20 12:54:42 +0530

    Added loaders

---
+++ src/Sample.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sample = () =>{
+  return(
+    <div className="loader-div">
+      <div className="loader">
+        
+      </div>
+    </div>
+  )
+}
+ export default Sample
